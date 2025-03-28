d6721ec119e57d5aa5d2309ca36af4226717b512
gvacosta <gvacosta@espol.edu.ec>
2025-03-28 12:48:54 -0500

    Se añade etiq_data.

---
+++ R/etiq_data.R
@@ -0,0 +1,14 @@
+#' Labeling of environmental data sets and phytoplankton species **env_data**, **spe_data** provided by ocean monitoring in Ecuador
+#'
+#'Location and time of measurement of physico-chemical and biological variables.
+#'Data sorted according to [env_data] and [spe_data] rows.
+#'
+#' @format ## `etiq_data`
+#' A data frame with 44 rows and 3 columns:
+#' \describe{
+#'   \item{ID}{Row identifier.}
+#'   \item{space}{Location from which the data were taken: 4 provinces of Ecuador.}
+#'   \item{month}{Month when measurement was made.}
+#' }
+#' @source Oceanographic Institute of the Navy of Ecuador (INOCAR).
+"etiq_data"
