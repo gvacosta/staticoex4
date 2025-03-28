8f8ee1894e8274582e6cbca31aa5251c7a842833
gvacosta <gvacosta@espol.edu.ec>
2025-03-28 12:23:02 -0500

    Se añade spe_data.

---
+++ R/spe_data.R
@@ -0,0 +1,35 @@
+#' Phytoplankton species data provided by ocean monitoring in Ecuador
+#'
+#' Measurement of the abundance <cells L-1> of 23 phytoplankton species
+#' from sampling data at four stations located 10 miles from
+#' the coastal profile of the continental equator.
+#'
+#' @format ## `spe_data`
+#' A data frame with 44 rows and 23 columns:
+#' \describe{
+#'   \item{e1}{affinis (*Chaetoceros*)}
+#'   \item{e2}{curvisetus (*Chaetoceros*)}
+#'   \item{e3}{didymus (*Chaetoceros*)}
+#'   \item{e4}{sinensis (*Hemiaulus*)}
+#'   \item{e5}{danicus (*Leptocylindrus*)}
+#'   \item{e6}{brightwellii (*Ditylum*)}
+#'   \item{e7}{fragilissimus (*Dactyliosolen*)}
+#'   \item{e8}{striata (*Guinardia*)}
+#'   \item{e9}{imbricata (*Rhizosolenia*)}
+#'   \item{e10}{alata (*Proboscia*)}
+#'   \item{e11}{antarcticus (*Dactyliosolen*)}
+#'   \item{e12}{costatum (*Skeletonema*)}
+#'   \item{e13}{borealis (*Lauderia*)}
+#'   \item{e14}{sp. (*Thalassiosira*)}
+#'   \item{e15}{sp. (*Nitzschia*)}
+#'   \item{e16}{longissima (*Nitzschia*)}
+#'   \item{e17}{pungens (*Pseudo-nitzschia*)}
+#'   \item{e18}{sp. (*Navicula*)}
+#'   \item{e19}{membranacea (*Stauroneis*)}
+#'   \item{e20}{nitzschioides (*Thalassionema*)}
+#'   \item{e21}{sp. (*Gymnodinium*)}
+#'   \item{e22}{sp. (*Gyrodinium*)}
+#'   \item{e23}{rubrum (*Mesodinium*)}
+#' }
+#' @source Oceanographic Institute of the Navy of Ecuador (INOCAR)
+"spe_data"
