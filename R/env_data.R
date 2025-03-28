c9275f4eca044d4840e09bbb6a79ecaf7b93652b
gvacosta <gvacosta@espol.edu.ec>
2025-03-28 11:50:27 -0500

    Se añade el primer dataset env_data.

---
+++ R/env_data.R
@@ -0,0 +1,18 @@
+#' Environmental variable data provided by ocean monitoring in Ecuador
+#'
+#' Environmental data from physical-chemical and biological data sampling at four stations
+#' located 10 miles from the coastal profile of continental Ecuador.
+#'
+#' @format ## `env_data`
+#' A data frame with 44 rows and 7 columns:
+#' \describe{
+#'   \item{T}{Temperature, ºC}
+#'   \item{S}{Salinity, ups}
+#'   \item{OD}{Dissolved oxygen dissolved, mg.L-1}
+#'   \item{NO2}{Nitrite, ug-at.L-1}
+#'   \item{NO3}{Nitrate, ug-at.L-1}
+#'   \item{PO4}{phosphate, ug-at.L-1}
+#'   \item{SiO4}{Silicate, ug-at.L-1}
+#' }
+#' @source Oceanographic Institute of the Navy of Ecuador (INOCAR)
+"env_data"
